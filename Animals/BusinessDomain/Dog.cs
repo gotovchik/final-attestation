4d96146327fe67c41b6b9f816219f4e0ec0d6b6d
Игорь Готовчик <gotovchik.igor@yandex.ru>
2023-06-02 12:43:37 +0300

    заполнение БД начальными данными

--- Animals/BusinessDomain/Dog.cs
+++ Animals/BusinessDomain/Dog.cs
@@ -5,6 +5,6 @@ namespace BusinessDomain
     public Guid id { get; set; }
     public string? Name { get; set; }
     public DateOnly? Birthday { get; set; }
-    public List<Command> Commands { get; set; }
+    public ICollection<Command> Commands { get; set; } = new List<Command>();
   }
 }
--- Animals/Persistence/AnimalsContext.cs
+++ Animals/Persistence/AnimalsContext.cs
@@ -5,12 +5,12 @@ namespace Persistence
 {
   public class AnimalsContext : DbContext
   {
-    public DbSet<Cat> Cats { get; set; }
-    public DbSet<Dog> Dogs { get; set; }
-    public DbSet<Hamster> Hamsters { get; set; }
-    public DbSet<Donkey> Donkeys { get; set; }
-    public DbSet<Horse> Horses { get; set; }
-    public DbSet<Command> Commands { get; set; }
+    public DbSet<Cat> Cats { get; set; } = null!;
+    public DbSet<Dog> Dogs { get; set; } = null!;
+    public DbSet<Hamster> Hamsters { get; set; } = null!;
+    public DbSet<Donkey> Donkeys { get; set; } = null!;
+    public DbSet<Horse> Horses { get; set; } = null!;
+    public DbSet<Command> Commands { get; set; } = null!;
 
     public AnimalsContext(DbContextOptions<AnimalsContext> opt) : base(opt)
     {
